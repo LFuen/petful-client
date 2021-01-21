ba2668161b397ad4476cbb31b6315bf1f3b6be6c
Lili Fuentes <lile7886@gmail.com>
2021-01-21 11:45:18 -0500

    new build

---
+++ build/precache-manifest.d4eba92b4c4bbd6c98be362bcd4cbc5c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "444c04360bf03fe096cbb5fd69f2af9b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4e4a09cc718522a10af",
+    "url": "/static/css/main.878fa7e2.chunk.css"
+  },
+  {
+    "revision": "65268ffa098e8b149a1e",
+    "url": "/static/js/2.5fa7428e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.5fa7428e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4e4a09cc718522a10af",
+    "url": "/static/js/main.34bd5979.chunk.js"
+  },
+  {
+    "revision": "10873a0725b3f95f5483",
+    "url": "/static/js/runtime-main.ee0db9a4.js"
+  },
+  {
+    "revision": "75e3a121c6bf5547b12747ac33a98721",
+    "url": "/static/media/adoptDog.75e3a121.png"
+  },
+  {
+    "revision": "11133177216b08bc1318be29e7ad7ffa",
+    "url": "/static/media/congrats.11133177.png"
+  }
+]);
