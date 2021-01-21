9b59d57dc92e876cef9b177eb361584c3d4af115
Lili Fuentes <lile7886@gmail.com>
2021-01-21 13:06:31 -0500

    cdm

--- src/Routes/Adoption.js
+++ src/Routes/Adoption.js
@@ -14,6 +14,7 @@ class AdoptionPage extends Component {
 
   componentDidMount() {
     this.context.clearError();
+    this.context.clearQueue();
     this.context.clearCurrentCat();
     this.context.clearCurrentDog();
 
