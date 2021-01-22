e20346080714b5f0842263530d927b293ffad4ee
Lili Fuentes <lile7886@gmail.com>
2021-01-21 19:32:36 -0500

    changed switch to browswerrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Route, Switch } from "react-router-dom";
+import { Route, BrowserRouter } from "react-router-dom";
 import Main from "./Routes/Main";
 import Adoption from "./Routes/Adoption";
 
