1c5245b3af9193a2e0c286c645ec79b91bf57b23
Lili Fuentes <lile7886@gmail.com>
2021-01-22 11:14:02 -0500

    browserrouter

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ export default class App extends Component {
     return (
       <div className="App">
         <main role="main" className="App-main">
-          <Switch>
+          <BrowserRouter>
             <Route exact path="/" component={Main} />
             <Route exact path="/adopt" component={Adoption} />
-          </Switch>
+          </BrowserRouter>
         </main>
       </div>
     );
